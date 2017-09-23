185dd0f1d973c3d5df737509aaef61e0f5bfc7ae
cqdonny <c_qdong@163.com>
2017-09-23 14:58:49 +0800

    program enviornment

--- manage.py
+++ manage.py
@@ -5,6 +5,19 @@ from app import create_app, db
 from flask_migrate import Migrate, MigrateCommand
 from flask_script import Shell, Manager
 # from app.models import User, Role, Post
+import config
+
+path_env = os.path.join(config.basedir, '.env')
+if os.path.exists(path_env):
+    print('Importing environment from.env...')
+    for line in open(path_env):
+        var = line.strip().split('=')
+        if len(var) == 2:
+            os.environ[var[0]] = var[1]
+
+    if os.environ.get('FLASK_CONFIG') == 'production':
+        config.ProductionConfig.SQLALCHEMY_DATABASE_URI = os.environ.get('PRO_DATABASE_URL')
+        config.Config.SECRET_KEY = os.environ.get('SECRET_KEY')
 
 app = create_app(os.environ.get('FLASK_CONFIG'))
 
