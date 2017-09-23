a69460b63a52536051e4496e8260698bb12d711e
cqdonny <c_qdong@163.com>
2017-09-23 13:58:51 +0800

    edit manage file

--- manage.py
+++ manage.py
@@ -1,12 +1,21 @@
 #-*- coding:utf-8 -*-
 
-from flask import Flask
+import os
+from app import create_app, db
+from flask_migrate import Migrate, MigrateCommand
+from flask_script import Shell, Manager
+# from app.models import User, Role, Post
 
-app = Flask(__name__)
+app = create_app(os.environ.get('FLASK_CONFIG'))
 
-@app.route('/')
-def index():
-    return '<h1>hello world!</h1>'
+manager = Manager(app)
+migrate = Migrate(app)
+
+def make_shell_context():
+    return dict(app=app, db=db)
+
+manager.add_command('shell', Shell(make_context=make_shell_context))
+manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    app.run()
+    manager.run()
