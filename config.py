65f17c166101dc6a6a86fb8238c0dca628863f4b
cqdonny <c_qdong@163.com>
2017-09-23 10:29:47 +0800

    first commit

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL')
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ.get('PRO_DATABASE_URL')
+
+class TestConfig(Config):
+    pass
+
+
+config = {
+    'default': DevelopmentConfig,
+    'development': DevelopmentConfig,
+    'production': ProductionConfig,
+    'test': TestConfig
+}
