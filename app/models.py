2f49fde5e613307db53872d2176b95474c222445
cqdonny <c_qdong@163.com>
2017-09-25 21:34:24 +0800

    add posts models

---
+++ app/models.py
@@ -0,0 +1,59 @@
+from flask import current_app, request, url_for
+from datetime import datetime
+from . import db
+from markdown import markdown
+import bleach
+from bs4 import BeautifulStoneSoup as bs
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128))
+    body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
+    body_digest = db.Column(db.Text)
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    view = db.Column(db.Integer, default=0)
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiatior):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+                        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'p', 'img']
+        attr = {
+            '*': ['class'],
+            'a': ['href', 'rel'],
+            'img': ['src', 'alt']
+        }
+        target.body_html = bleach.linkify(bleach.clean(
+            markdown(value, output_format='html', extensions=['markdown.extensions.extra']),
+            tags=allowed_tags, strip=True,attributes=attr
+        ))
+
+        num = 1
+        tags = ''
+        tags_begin = False
+        abstract = ''
+        for i in target.body_html:
+            if num <= 300:
+                if i == '<':
+                    tags_begin = True
+                elif i == '>':
+                    tags += i
+                    abstract += tags
+                    tags = ''
+                    tags_begin = False
+                    continue
+                if tags_begin:
+                    tags += i
+                else:
+                    abstract += i
+                    num += 1
+        target.body_digest = bs(abstract, 'html.parser').prettify()
+
+        def views(self):
+            self.view += 1
+            db.session.add(self)
+
+        def __repr__(self):
+            return '<POST %r>' %self.title
--- config.py
+++ config.py
@@ -5,6 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
 
     @staticmethod
     def init_app(app):
