704c8616e05350f51c0102bdcb2f87a8d0b6920b
cqdonny <c_qdong@163.com>
2017-10-12 22:16:56 +0800

    网页部局修改

--- app/main/views.py
+++ app/main/views.py
@@ -11,7 +11,7 @@ from flask_login import login_required, current_user
 @main.route('/')
 def index():
     page = request.args.get('page', 1, type=int)
-    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=3, error_out=False)
+    pagination = Post.query.order_by(Post.timestamp.desc()).paginate(page, per_page=5, error_out=False)
     post = pagination.items
     # post = Post.query.order_by(Post.timestamp.desc())
     return render_template('index.html', posts=post, utctime=datetime.utcnow(), pagination=pagination)
